f414548d0b5ed5398a64a2f07633bf8a22fbb5f6
Raj Kaste <rajkaste@gmail.com>
2020-10-14 13:05:40 +0530

    Create Avengers.java

---
+++ Day 4/Avengers.java
@@ -0,0 +1,37 @@
+package com.letsupgrade;
+
+import java.util.Scanner;
+
+public class Avengers {
+
+    Scanner sc = new Scanner(System.in);
+
+    String name,power,weapon,planet;
+    int age;
+
+    public void getDetails(){
+
+        System.out.print("Avenger Name: ");
+        name = sc.nextLine();
+        System.out.print("Enter the Power: ");
+        power = sc.nextLine();
+        System.out.print("Enter the Weapon: ");
+        weapon = sc.nextLine();
+        System.out.print("Enter the Planet/Realm: ");
+        planet = sc.nextLine();
+        System.out.print("Enter the age: ");
+        age = sc.nextInt();
+
+    }
+
+    public void displayDetails(){
+
+        System.out.println("Avenger: "+name);
+        System.out.println("Power: "+power);
+        System.out.println("Weapon: "+weapon);
+        System.out.println("Age: "+age);
+        System.out.println("Planet: "+planet);
+
+    }
+
+}
