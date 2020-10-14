d55b04337783a1487b525e0eb2323d613493d838
Raj Kaste <rajkaste@gmail.com>
2020-10-14 12:09:01 +0530

    Update OddNos.java

--- Day 4/OddNos.java
+++ Day 4/OddNos.java
@@ -3,16 +3,16 @@ package com.letsupgrade;
 public class OddNos {
     public static void main(String[] args) {
 
-        int[] arr = new int[6];
+        int[] arr = new int[5];
 
         System.out.println("Some nos: ");
-        for (int i=1;i<arr.length;i++){
-            arr[i] = i;
+        for (int i=0;i<arr.length;i++){
+            arr[i] = i+1;
             System.out.print(arr[i]+" ");
         }
-        
+
         System.out.println("\nOdd nos: ");
-        for (int i=1;i<arr.length;i++){
+        for (int i=0;i<arr.length;i++){
             if (arr[i]%2!=0){
                 System.out.print(arr[i]+" ");
             }
