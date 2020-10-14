eef19d5f70c4539102c614c411ad752e9f790537
Raj Kaste <rajkaste@gmail.com>
2020-10-14 11:47:34 +0530

    Create SumArray.java

---
+++ Day 4/SumArray.java
@@ -0,0 +1,24 @@
+package com.letsupgrade;
+
+import java.util.Scanner;
+
+public class SumArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum=0;
+
+        System.out.println("Enter 5 nos.:");
+        for (int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+
+        System.out.print("Sum of all nos.: "+sum);
+
+    }
+}
