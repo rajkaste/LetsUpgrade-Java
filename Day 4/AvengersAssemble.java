f3a46a5a8b439e215f20da66ab39910c3dab60ba
Raj Kaste <rajkaste@gmail.com>
2020-10-14 13:06:06 +0530

    Create AvengersAssemble.java

---
+++ Day 4/AvengersAssemble.java
@@ -0,0 +1,22 @@
+package com.letsupgrade;
+
+public class AvengersAssemble {
+    public static void main(String[] args) {
+
+        Avengers[] avgs = new Avengers[5];
+
+        System.out.println("Avengers Initiative:");
+        for (int i=0;i< avgs.length;i++){
+            avgs[i] = new Avengers();
+            avgs[i].getDetails();
+            System.out.println();
+        }
+        System.out.println("Avengers Assemble...");
+        for (int i=0;i< avgs.length;i++){
+            System.out.println();
+            avgs[i].displayDetails();
+            System.out.println();
+        }
+
+    }
+}
